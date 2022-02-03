5aee85532e17ae741c3eb9767ccf224277db4b20
ads226 <ads226@naver.com>
2022-02-03 23:12:38 +0900

    QBicUISwitches
    version 1.0.0

--- components/QBicConponents.js
+++ components/QBicConponents.js
@@ -1,5 +1,7 @@
 import QBicUICheckers from './js/ui/QBicUICheckers.js';
+import QBicUISwitches from './js/ui/QBicUISwitches.js';
 import QBicUIToggles from './js/ui/QBicUIToggles.js';
 
 customElements.define(QBicUICheckers.is, QBicUICheckers);
 customElements.define(QBicUIToggles.is, QBicUIToggles);
+customElements.define(QBicUISwitches.is, QBicUISwitches);
